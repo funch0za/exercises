9c37a4346073944c7916ab8976f5bb9294fd55fe
Boil Britt <boilbrit@mi-a2.msk.21-school.ru>
2024-06-21 20:20:31 +0300

    ex23

--- solutions/chapter1/ex22.c
+++ solutions/chapter1/ex22.c
@@ -2,22 +2,39 @@
 
 #define N 3
 
+// balabalabbala
+
+
+/*
+adadmaksnkjfnjksdnfs
+*/
+
 int main() {
   int endline = 0;
   char c;
+
+  int a = 5 * 8 / 2;
+
   while ((c = getchar()) != EOF) {
     if (c == '\n') {
       endline = 0;
       putchar('\n');
+      /*
+      ddddd*/
     } else if (endline >= N) {
-      if (c != ' ' && c != '\t') {
+      if (c != ' ' && c != '\t') { /*ababbababababababab*/
         endline = 1;
         putchar('\n');
       }
+      //1
+      //2
+      //
+
+      //
       putchar(c);
     } else {
       ++endline;
-      putchar(c);	
+      putchar(c);	// abbabab
     }
   }
 }
---
+++ solutions/chapter1/ex23.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define OUT 0
+#define ONE_LINE_COMMENT 1
+#define MULTILINE_COMMENT 2
+
+int main() {
+  int state = OUT;
+  char a, b, c;
+  a = EOF;
+  b = EOF;
+  c = EOF;
+  while ((c = getchar()) != EOF) {
+    if (b != EOF) {
+        switch (state) {
+            case ONE_LINE_COMMENT:
+                if (c == '\n') {
+                    state = OUT;
+                    putchar('\n');
+                }
+                break;
+            case MULTILINE_COMMENT:
+                if (b == '*' && c == '/') {
+                    state = OUT;
+                }
+                break;
+            case OUT:
+                if (b == '/' && c == '/') {
+                    state = ONE_LINE_COMMENT;
+                } else if (b == '/' && c == '*') {
+                    state = MULTILINE_COMMENT;
+                } else {
+                    if (c != '/') {
+                        putchar(c);
+                    }
+                    if (b == '/') {
+                        if (a != EOF && a != '*') {
+                            putchar(b);
+                        }
+                        putchar(c);
+                    }
+                }
+                break;
+        }
+    }
+
+    a = b;
+    b = c;
+   }
+}
