b04b126d3055bbdf7f5d5d9990c1c9bd9d5450ad
Nadezhda Samartseva, boilbrit, funch0za <nadya.sm101@yandex.ru>
2024-06-29 00:48:17 +0300

    ex3

--- solutions/chapter2/ex3.c
+++ solutions/chapter2/ex3.c
@@ -1,7 +1,38 @@
-#define MAX 1000
+#include <string.h>
+#include <stdio.h>
 
-void htoi(char s[MAX]) {
-}
+#define N 3
+
+int htoi(char s[]);
 
 int main() {
+	char s1[] = "AAA";
+	char s2[] = "0x888";
+	char s3[] = "0xFFA10";
+
+	char* all[N] = {s1, s2, s3};
+
+	for (int i = 0; i < N; ++i) {
+		puts(all[i]);
+		printf("%u\n\n", htoi(all[i]));	
+	}
+}
+
+int htoi(char s[]) {
+	int result = 0, stop = 0;
+	if (s[0] == '0' && (s[1] == 'x' || s[0] == 'X')) {
+		stop = 2;
+	}
+
+	for (int i = strlen(s) - 1, pow = 1; i >= stop; --i, pow *= 16) {
+		if ('a' <= s[i] && s[i] <= 'z') {
+			result += (s[i] - 'a' + 10) * pow;
+		} else if ('A' <= s[i] && s[i] <= 'Z') {
+			result += (s[i] - 'A' + 10) * pow;
+		} else {
+			result += (s[i] - '0') * pow;
+		}
+	}
+
+	return result;
 }
