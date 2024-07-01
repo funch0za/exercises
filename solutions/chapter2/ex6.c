830627c53fc48c908c0a0d80aa0c4dd1534ecbd8
Nadezhda Samartseva, boilbrit, funch0za <nadya.sm101@yandex.ru>
2024-07-02 00:03:41 +0300

    ex6

--- solutions/chapter2/ex6.c
+++ solutions/chapter2/ex6.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-unsigned getbits(unsigned x, int p, int n);
-unsigned setbits(unsigned x, int p, int n, unsigned y);
-void print_bit(unsigned x);
-
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
 	unsigned mask = (~(unsigned)0) >> p;
 	mask <<= p;
