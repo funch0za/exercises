c19e1c2847b4edac790349791b906eaafb79ace5
Nadezhda Samartseva, boilbrit, funch0za <nadya.sm101@yandex.ru>
2024-07-01 23:50:27 +0300

    ex6

--- solutions/chapter2/ex6.c
+++ solutions/chapter2/ex6.c
@@ -1,9 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 unsigned getbits(unsigned x, int p, int n);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+void print_bit(unsigned x);
 
-int main() {
-
-}
-
-unsigned getbits(unsigned x, int p, int n) {
-	return (x >> (p + 1 - n)) & ~(~0 << n);
+unsigned setbits(unsigned x, int p, int n, unsigned y) {
+	unsigned mask = (~(unsigned)0) >> p;
+	mask <<= p;
+	mask >>= (p - n);
+	mask = ~mask;
+	mask <<= (p - n);
+	mask = ~mask;
+	x &= mask;
+	y &= ~(~0 << n);
+	y <<= (p - n);
+	x |= y;
+	return x;
 }
