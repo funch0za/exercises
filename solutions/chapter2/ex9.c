9528bbeb5f45468dd350f9e039251e51d84bebda
Nadezhda Samartseva, boilbrit, funch0za <nadya.sm101@yandex.ru>
2024-07-15 16:05:18 +0300

    ex9

---
+++ solutions/chapter2/ex9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int bitcount_1(unsigned);
+int bitcount_2(unsigned);
+
+int main() {
+	for (unsigned i = 0; i <= 100; ++i) {
+		int b1 = bitcount_1(i), b2 = bitcount_2(i);
+		printf("i = %u\tbitcount_1 = %d\tbitcount_2 = %d\n", i, b1, b2);
+		if (b1 != b2) {
+			puts("ERROR");
+		}
+	}
+}
+
+int bitcount_1(unsigned x) {
+	int b = 0;
+	for (; x != 0; x >>= 1) {
+		if (x & 1) {
+			++b;
+		}
+	}
+	return b;	
+}
+
+int bitcount_2(unsigned x) {
+	int b = 0;
+	while (x) {
+		x &= (x - 1);
+		++b;
+	}
+	return b;
+}
